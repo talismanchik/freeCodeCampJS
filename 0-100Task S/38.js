0b21d20e06aeafe0b38bba2a030582b9d21cfb4f
talismanchik <talismanchik.by@gmail.com>
2022-10-29 21:24:10 +0300

    add 38 task

--- 0-100Task S/38.js
+++ 0-100Task S/38.js
@@ -1,1 +1,8 @@
+const myNoun = "dog";
+const myAdjective = "big";
+const myVerb = "ran";
+const myAdverb = "quickly";
 
+// Only change code below this line
+const wordBlanks = myNoun + " " + myAdjective + " " + myVerb + " " + myAdverb + " " + "world"; // Change this line
+// Only change code above this line
